25f1b21a4f9756a7a7b99a4e5b911b7da6188607
Bruno Egremy <32512987+egremyb@users.noreply.github.com>
2020-05-03 17:52:36 +0200

    Added function that returns animals on GET requests : NOT TESTED

--- docker-images/express-image/src/index.js
+++ docker-images/express-image/src/index.js
@@ -1,4 +1,53 @@
 var Chance = require('chance');
 var chance = new Chance();
 
-console.log("Dang ! You're such a " + chance.animal());
+const express = require('express');
+const app = express();
+
+app.get('/', function (req, res) {
+    res.send(generateAnimals);
+});
+
+app.listen(3000, function () {
+    console.log("Dang an HTTP request ! You're such a " + chance.animal());
+});
+
+function generateAnimals() {
+    var numberOfAnimals = chance.integer({
+        min: 1,
+        max: 10
+    });
+
+    console.log(numberOfAnimals);
+
+    var animals = [];
+    for (var i = 0; i < numberOfAnimals; i++){
+        var animal = chance.animal();
+        var weight = chance.floating({
+            min: 0.01,
+            max: 1000
+        });
+
+        var syllables = chance.integer({
+            min: 1,
+            max: 3
+        });
+
+        var battleCry = "";
+
+        for (var i = 0; i < syllables; i++){
+            battleCry += chance.syllable();
+        }
+
+        animals.push({
+            specie: animal,
+            weight: weight,
+            battleCry: battleCry
+        })
+    }
+
+    console.log(animals);
+
+    return animals;
+}
+
