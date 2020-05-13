47658efe2e07b778954bffd7ca38b3fed9709c76
Bruno Egremy <32512987+egremyb@users.noreply.github.com>
2020-05-13 20:26:26 +0200

    Transfert -> laptop

--- docker-images/traefik-image/traefik_run.sh
+++ docker-images/traefik-image/traefik_run.sh
@@ -1,1 +1,3 @@
-docker run -d -v /var/run/docker.sock:/var/run/docker.sock -v $PWD/traefik.toml:/traefik.toml -p 80:80 -l traefik.frontend.rule=Host:monitor.labo.res.ch -l traefik.port=8080 --network res_http_lab_network --name traefik_rp traefik:2.2.1
+docker build -t res/traefik_reverse_proxy .
+docker run -d --name traefik_rp -p 80:80 -p 8081:8080 -v /var/run/docker.sock:/var/run/docker.sock res/traefik_reverse_proxy
+docker-compose up -d --build
